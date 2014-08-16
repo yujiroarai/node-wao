c96b3e08b6d1a37788446b5b060af60e99511363
yujiroarai <yujiroarai@gmail.com>
2014-08-16 17:54:08 +0900

    mongooseを使ってmongoとのやりとりができた

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-var http = require('http'), fs = require('fs');
-var jsdom = require("jsdom");
-var $ = require("jquery")(jsdom.jsdom().createWindow());
+var http = require('http'),
+  fs = require('fs'),
+  jsdom = require("jsdom"),
+  mongoose = require('mongoose'),
+  $ = require("jquery")(jsdom.jsdom().createWindow()),
+  mongo = require('mongodb');
+
+// mongoose
+var Schema   = mongoose.Schema;
+var ShopSchema = new Schema({
+  name:  String,
+  address: String
+});
+mongoose.model('Shop', ShopSchema);
+mongoose.connect('mongodb://localhost/wao');
+
+// mongodb
+// var db = new mongo.Db('wao', new mongo.Server('localhost',mongo.Connection.DEFAULT_PORT, {}), {});
 
 http.createServer(function (request, response) {
 
-  fs.readFile(__dirname + '/hello.html', function (err, data) {
+  fs.readFile(__dirname + '/bind_out.html', function (err, data) {
     if (err) throw err;
+
+    var body = $(data.toString());
+    var wao_script = body.find('script[type="text/wao"]');
+console.log('00000000000000000000');
+eval(wao_script.html());
+console.log('00000000000000000000');
+    // body.find('[data-bind-out]').each(function(){
+    //   var bind_out = JSON.parse($(this).attr('data-bind-out'));
+    // });
 
     var body = data.toString();
     var title = $(body).html();
