728411cac3e676b21059f9700930a2deb79f48bd
Manabu Kimura <m_kimura@headwaters.co.jp>
2014-09-24 06:44:22 +0900

    data-wao-bindを可能にした
    ・complate.html：バインド用に入力した値を表示するためのtableを追加
    ・app.js：POST, GET, テンプレ読み込みを同期処理に変更、data-wao-bindによるバインド処理を追加

--- app.js
+++ app.js
@@ -32,38 +32,38 @@ http.createServer(function(request, response) {
   console.log('');
   var waoPage = WaoPageFactory();
   // methodで処理を切り分け
-  // POSTメソッド・・・データの追加
-  if (request.method == 'POST') {
-    waoPage.postData(request);
-  }
   // PUTメソッド・・・データの変更
   // DELETEメソッド・・・データの削除
-  // GETメソッド・・・データの取得
-  if (request.method == 'GET') { // TODO：POSTとかの時もクエリストリング使えるようにしたい
-    waoPage.getData(request);
-  }
-  // テンプレートを読み込む
-  waoPage.readTemplate(request, function(err, data) {
+  // POSTメソッド・・・データの追加
+  waoPage.postData(request, function(err, data) {
     if (err) throw err;
-    // レスポンス出力
-    var status = waoPage.getStatusCode();
-    var mimeType = waoPage.getMimeType();
-    if (status == 200) {
-      console.log('Start response. statusCode=' + status + ', mimeType="' + mimeType + '"');
-      response.writeHead(status, { 'Content-Type': mimeType });
-      response.end(waoPage.getResponseData());
-    } else if (status == 301) {
-      var redirectUrl = 'http://localhost:8888' + waoPage.getRedirectUrl(); // TODO：ホスト名とか
-      console.log('Start response. statusCode=' + status + ', Location="' + redirectUrl + '"');
-      response.writeHead(status, { 'Location': redirectUrl });
-      response.end();
-    } else {
-      console.log('Start response. statusCode=' + status);
-      response.statusCode = (status);
-      response.end();
-    }
-    console.log('------------------------------');
-    console.log('');
+    // GETメソッド・・・データの取得
+    waoPage.getData(request, function(err, data) {
+      if (err) throw err;
+      // テンプレートを読み込む
+      waoPage.readTemplate(request, function(err, data) {
+        if (err) throw err;
+        // レスポンス出力
+        var status = waoPage.getStatusCode();
+        var mimeType = waoPage.getMimeType();
+        if (status == 200) {
+          console.log('Start response. statusCode=' + status + ', mimeType="' + mimeType + '"');
+          response.writeHead(status, { 'Content-Type': mimeType });
+          response.end(waoPage.getResponseData());
+        } else if (status == 301) {
+          var redirectUrl = 'http://localhost:8888' + waoPage.getRedirectUrl(); // TODO：ホスト名とか
+          console.log('Start response. statusCode=' + status + ', Location="' + redirectUrl + '"');
+          response.writeHead(status, { 'Location': redirectUrl });
+          response.end();
+        } else {
+          console.log('Start response. statusCode=' + status);
+          response.statusCode = (status);
+          response.end();
+        }
+        console.log('------------------------------');
+        console.log('');
+      });
+    })
   });
 }).listen(process.env.PORT || 8888);
 
@@ -78,71 +78,101 @@ var WaoPageFactory = function() {
     // TODO：リファラからテンプレートを選択してフォームの整合性をチェックする処理を追加する
     // ・余計なフォームが送信されて来ていないか？
     // ・html5 form validationと同じ内容のサーバー側バリデーション処理
-    postData : function(request) {
-      var me = this;
-      var data = '';
-      request.on('data', function(chunk) {
-        data += chunk;
-      });
-      request.on('end', function() {
-        if (!me.crudData) me.crudData = { find : {}, insert: {} };
-        var collectionName;
-        // POSTデータをJSON化
-        var query = querystring.parse(data);
-        // JSON化したPOSTデータをmongoDBに入れられるJSON形式
-        for (var key in query) {
-          // <input name="collactionName.propertyName">
-          collectionName = key.match(/^([^.]+)\./)[1]; // TODO：collectionの決定方法がアホ
-          if (!me.crudData.insert[collectionName]) {
-            var createdDate = new Date().getTime();
-            me.crudData.insert[collectionName] = {
-              id: (createdDate + Math.random() + '').replace('.', ''),
-              _createdDate: createdDate
-            };
+    postData : function(request, callback) {
+      if (request.method == 'POST') {
+        var me = this;
+        var data = '';
+        request.on('data', function(chunk) {
+          data += chunk;
+        });
+        request.on('end', function() {
+          if (!me.crudData) me.crudData = { find : {}, insert: {} };
+          var collectionName;
+          // POSTデータをJSON化
+          var query = querystring.parse(data);
+          // JSON化したPOSTデータをmongoDBに入れられるJSON形式に変換
+          for (var key in query) {
+            // <input name="collactionName.propertyName">
+            collectionName = key.match(/^([^.]+)\./)[1]; // TODO：collectionの決定方法がアホ
+            if (!me.crudData.insert[collectionName]) {
+              var createdDate = new Date().getTime();
+              me.crudData.insert[collectionName] = {
+                id: (createdDate + Math.random() + '').replace('.', ''),
+                _createdDate: createdDate
+              };
+            }
+            me.crudData.insert[collectionName][key.replace(collectionName + '.', '')] = query[key];
           }
-          me.crudData.insert[collectionName][key.replace(collectionName + '.', '')] = query[key];
-        }
-        // mongoDBにデータを登録
-        for (var colName in me.crudData.insert) {
-          db.collection(colName, function(err, collection) {
-            collection.insert(me.crudData.insert[colName], function() {
-              console.log('WaoPage.postData() : Success to insert a new data.');
-              console.log(me.crudData.insert[colName]);
-              console.log('');            });
-          });
-        }
-      });
+          // mongoDBにデータを登録
+          var colCount = 0;
+          var maxColCount = Object.keys(me.crudData.insert).length;
+          if (maxColCount == 0) {
+            callback(null, null);
+          } else {
+            for (var colName in me.crudData.insert) {
+              db.collection(colName, function(err, collection) {
+                collection.insert(me.crudData.insert[colName], function() {
+                  console.log('WaoPage.postData() : Success to insert a new data.');
+                  console.log(me.crudData.insert[colName]);
+                  console.log('');
+                  colCount++;
+                  if (colCount == maxColCount) {
+                    callback(null, null);
+                  }
+                });
+              });
+            }
+          }
+        });
+      } else {
+        callback(null, null);
+      }
     },
     // GETメソッドあるいはクエリストリングの処理（データの取得）
     // TODO：postData()ともう少し共通化できると思う
-    getData : function(request) {
-      var me = this;
-      if (!me.crudData) me.crudData = { find : {}, insert: {} };
-      if (!me.findData) me.findData = {}; // 初期化の場所を考えなくては。。。
-      var url_parts = url.parse(request.url, true);
-      me.crudData.find = url_parts.query;
+    getData : function(request, callback) {
+      if (request.method == 'GET') {
+        var me = this;
+        if (!me.crudData) me.crudData = { find : {}, insert: {} };
+        if (!me.findData) me.findData = {}; // TODO:初期化の場所を考えなくては。。。
+        var url_parts = url.parse(request.url, true);
+        me.crudData.find = url_parts.query;
 
-      var collectionName;
-      // POSTデータをJSON化
-      var query = me.crudData.find;
-      // JSON化したPOSTデータをmongoDBに入れられるJSON形式
-      for (var key in query) {
-        // <input name="collactionName.propertyName">
-        collectionName = key.match(/^([^.]+)\./)[1]; // TODO：collectionの決定方法がアホ
-        if (!me.crudData.find[collectionName]) me.crudData.find[collectionName] = {};
-        me.crudData.find[collectionName][key.replace(collectionName + '.', '')] = query[key];
-      }
-      // mongoDBからデータを取得
-      for (var colName in me.crudData.find) {
-        db.collection(colName, function(err, collection) {
-          collection.find(me.crudData.find[colName], {limit:1}).toArray(function(err, docs) {
-            console.log('WaoPage.getData() : Success to find data[s].');
-            console.log(me.crudData.find[colName]);
-            console.log(docs);
-            console.log('');
-            me.findData[colName] = docs[0];
-          });
-        });
+        var collectionName;
+        // GETパラメタ
+        var query = me.crudData.find;
+        // GETパラメタをmongoDBの検索条件に指定できるJSON形式に変換
+        for (var key in query) {
+          // ?collactionName.propertyName=xxxx
+          collectionName = key.match(/^([^.]+)\./)[1]; // TODO：collectionの決定方法がアホ
+          if (!me.crudData.find[collectionName]) me.crudData.find[collectionName] = {};
+          me.crudData.find[collectionName][key.replace(collectionName + '.', '')] = query[key];
+        }
+        // mongoDBからデータを取得
+        var colCount = 0;
+        var maxColCount = Object.keys(me.crudData.find).length;
+        if (maxColCount == 0) {
+          callback(null, null);
+        } else {
+          for (var colName in me.crudData.find) {
+            db.collection(colName, function(err, collection) {
+              collection.find(me.crudData.find[colName], {limit:1}).toArray(function(err, docs) {
+                console.log('WaoPage.getData() : Success to find data[s].');
+                console.log('  colName = "' + colName + '"');
+                console.log(me.crudData.find[colName]);
+                console.log(docs);
+                console.log('');
+                me.findData[colName] = docs[0];
+                colCount++;
+                if (colCount == maxColCount) {
+                  callback(null, null);
+                }
+              });
+            });
+          }
+        }
+      } else {
+        callback(null, null);
       }
     },
     // URLルーティングの処理
@@ -199,9 +229,17 @@ var WaoPageFactory = function() {
     bind : function() {
       var $dom = $(this.responseData); // TODO：<!DOCTYPE>に対応できてない ＆ parseできない時の処理が必要
       var me = this;
+      // GETパラメタにバインド
       $dom.find('a').each(function(){
         $(this).attr('href', me.bindGetParam($(this).attr('href')));
       });
+      // data-wao-bind属性にバインド（innerHtml）
+      $dom.find('[data-wao-bind]').each(function(){
+        var val = $(this).attr('data-wao-bind');
+        $(this).html(me.getValue(val.split('.')[0], val.split('.')[1]));
+        $(this).removeAttr('data-wao-bind');
+      });
+      // TODO:each(function(){})を使ってるから同期が必要じゃ？？？
       this.responseData = $dom.selfHtml();
     },
     // URLに指定されたGETパラメタにデータをバインドする処理
@@ -228,12 +266,16 @@ var WaoPageFactory = function() {
     // 参照文字列から自動的に適切なオブジェクトを選択して、参照文字列に対応するデータを返却する
     getValue : function(col, prop) {
       // TODO：_DBとか_SESって修飾子がついていたら・・・的な処理が今後必要
-      var val;
+      var val = '（データが見つかりません）';
+      var key = 'undefined';
       if (this.crudData.insert[col] && this.crudData.insert[col][prop]) {
+        key = '_DB.' + col + '.' + prop;
         val = this.crudData.insert[col][prop];
       } else if (this.findData[col] && this.findData[col][prop]) {
+        key = '_DB.' + col + '.' + prop;
         val = this.findData[col][prop];
       }
+      console.log('getValue() : ' + key + '=' + val);
       return val;
     },
     // レスポンス出力処理
