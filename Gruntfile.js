ab874c7d1b4015d87718fbb3f9b0e1372111c78f
中村 一輝 <takumarakann@gmail.com>
2014-12-07 19:09:45 +0900

    ドキュメントサイトを作成

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    autoprefixer: {
+      target: {
+        expand: true,
+        flatten: true,
+        src: 'docs/src/**/*.css',
+        dest: 'docs/dest/css'
+      },
+      options: {
+        browsers: ['last 2 version', 'ie 8', 'ie 9']
+      }
+    },
+    cssmin: {
+      target: {
+        expand: true,
+        flatten: true,
+        src: ['docs/dest/css/**/*.css', '!*.min.css'],
+        dest: 'docs/dest/css'
+      }
+    },
+    jshint: {
+      options:{
+        jshintrc: '.jshintrc'
+      },
+      files: ['docs/src/js/**/*.js']
+    },
+    uglify: {
+      target: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: 'docs/src/**/*.js',
+          dest: 'docs/dest/js'
+        }]
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        flatten: true,
+        src: 'docs/src/**/*.html',
+        dest: 'docs/dest/'
+      },
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+        }
+      }
+    },
+    open: {
+      delayed: {
+        path: 'http://localhost:<%=connect.server.options.port%>/docs/src/index.html',
+        // path: 'http://localhost:<%=connect.server.options.port%>/docs/dest/index.html',
+      },
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      target: {
+        files: ['Gruntfile.js','docs/src/**/*'],
+        tasks: ['live']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['autoprefixer', 'cssmin', 'uglify', 'connect', 'open', 'copy', 'watch']);
+  grunt.registerTask('live', ['autoprefixer', 'cssmin', 'uglify', 'copy']);
+};
